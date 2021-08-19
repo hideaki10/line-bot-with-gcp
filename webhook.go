1f05ab1cf65c842b5776ddba771c87ee812b1b0e
Oracle Public Cloud User <opc@instance-20210819-1706.subnet05230010.vcn05230010.oraclevcn.com>
2021-08-19 14:37:17 +0000

    one file

--- webhook.go
+++ webhook.go
@@ -1,99 +1,45 @@
 package linebot
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
-	cloudkms "cloud.google.com/go/kms/apiv1"
-	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
-
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-var (
-	secrets Secrets
-)
+func Webook(w http.ResponseWriter, r *http.Request) {
 
-type Secrets struct {
-	LineChannelSecret      string `json:"line_channel_secret"`
-	LineChannelAccessToken string `json:"line_channel_access_token"`
-}
+	LineChannelSecret := os.Getenv("LINE_CHANNEL_SECRET")
+	LineChannelAccessToken := os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
 
-func init() {
-	secretsJson, err := decryptLineSecrets()
+	client, err := linebot.New(LineChannelSecret, LineChannelAccessToken)
 	if err != nil {
-		log.Fatal("failed decrypt secrets", err)
-		return
-	}
-	if err := json.Unmarshal(secretsJson, &secrets); err != nil {
-		log.Fatal("failed json unmarshal secrets", err)
-		return
-	}
-}
-
-func Webhook(w http.ResponseWriter, r *http.Request) {
-	client, err := linebot.New(secrets.LineChannelSecret, secrets.LineChannelAccessToken)
-	if err != nil {
-		http.Error(w, "Error init client", http.StatusBadRequest)
+		http.Error(w, "Error client", http.StatusBadRequest)
 		log.Fatal(err)
 		return
 	}
+
 	events, err := client.ParseRequest(r)
 	if err != nil {
 		http.Error(w, "Error parse request", http.StatusBadRequest)
 		log.Fatal(err)
 		return
 	}
+
 	for _, e := range events {
 		switch e.Type {
 		case linebot.EventTypeMessage:
-			message := linebot.NewTextMessage("Test")
+			message := linebot.NewTextMessage("test")
 			_, err := client.ReplyMessage(e.ReplyToken, message).Do()
 			if err != nil {
 				log.Println(err)
-				continue
+				return
 			}
 		}
 	}
-	fmt.Fprint(w, "ok")
-}
 
-func lineSecretsKmsKeyName() string {
-	prjID := os.Getenv("GCP_PROJECT_ID")
-	keyRingName := os.Getenv("KMS_KEY_RING_NAME")
-	keyName := os.Getenv("KMS_LINE_SECRETS_KEY_NAME")
-	return fmt.Sprintf("projects/%s/locations/global/keyRings/%s/cryptoKeys/%s", prjID, keyRingName, keyName)
-}
+	fmt.Fprintln(w, "ok")
 
-func decryptLineSecrets() ([]byte, error) {
-	enc, err := ioutil.ReadFile("secrets.json")
-	if err != nil {
-		return nil, err
-	}
-	return decryptSymmetric(lineSecretsKmsKeyName(), enc)
-}
-
-func decryptSymmetric(keyName string, ciphertext []byte) ([]byte, error) {
-	ctx := context.Background()
-	client, err := cloudkms.NewKeyManagementClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	// Build the request.
-	req := &kmspb.DecryptRequest{
-		Name:       keyName,
-		Ciphertext: ciphertext,
-	}
-	// Call the API.
-	resp, err := client.Decrypt(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-	return resp.Plaintext, nil
 }
